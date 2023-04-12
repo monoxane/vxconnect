2d7b492b42652375c2767c1c2403e2d09a090a68
Oliver <oliver@monoxane.io>
2023-04-12 17:20:29 +1000

    fix things smh

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,74 +1,128 @@
-const webpack = require('webpack');
+/* eslint-disable global-require */
 const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const config = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, '/dist'),
-    publicPath: '/',
-    filename: 'bundle.js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.ts(x)?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              mimetype: 'image/png'
-            }
-          }
-        ]
-      }
-    ]
-  },
-  devServer: {
-    compress: true,
-    historyApiFallback: true,
-    hot: true,
-    historyApiFallback: {
-        index: '/'
-    }
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><script src="/bundle.js"></script><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"root\"></div></body></html>',
-      filename: 'index.html',
-    }),
-    new LodashModuleReplacementPlugin,
-    new MiniCssExtractPlugin()
-  ],
-  resolve: {
-    extensions: [
-      '.tsx',
-      '.ts',
-      '.jsx',
-      '.js'
-    ]
-  }
-};
+const port = process.env.PORT || 3000;
 
-module.exports = config;
+module.exports = (_, argv) => {
+  const { mode } = argv;
+  const isDevelopment = mode === 'development';
+
+  return {
+    entry: [
+      './src/index.tsx',
+    ],
+
+    output: {
+      path: path.resolve(__dirname, 'dist/'),
+      publicPath: '/dist/',
+      filename: '[name].[contenthash].js',
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'VXConnect',
+        template: 'src/index.html',
+        filename: 'index.html',
+      }),
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+    ].filter(Boolean),
+
+    optimization: {
+      moduleIds: 'deterministic',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      },
+      usedExports: true,
+    },
+
+    performance: {
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000,
+    },
+
+    resolve: {
+      extensions: [
+        '.tsx',
+        '.ts',
+        '.jsx',
+        '.js'
+      ]
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                // this code will evaluate to "false" when
+                // "isDevelopment" is "false"
+                // otherwise it will return the plugin
+                isDevelopment && require('react-refresh/babel'),
+              // this line removes falsy values from the array
+              ].filter(Boolean),
+            },
+          },
+        },
+        {
+          test: /\.ts(x)?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.png$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                mimetype: 'image/png'
+              }
+            }
+          ]
+        },
+      ],
+    },
+
+    devtool: isDevelopment && 'inline-source-map',
+
+    devServer: isDevelopment ? {
+      host: '0.0.0.0',
+      port,
+      open: false,
+      allowedHosts: 'all',
+      historyApiFallback: {
+        index: '/dist/index.html',
+      },
+      magicHtml: true,
+      proxy: {
+        '/api/v1': {
+          target: 'https://connect.vx0.dev',
+        },
+      },
+    } : {
+
+    },
+  };
+};
