ef9c71fe51d90691e9bab35411c48afd09d03844
Alexander Cafarella <cafarellaalex@gmail.com>
2023-04-13 10:36:03 +1000

    Delete confirmation, Creation error.

--- frontend/src/Zones/Zones.jsx
+++ frontend/src/Zones/Zones.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
-import useAxiosPrivate, { getAxiosPrivate }  from '../hooks/useAxiosPrivate';
+import useAxiosPrivate, { getAxiosPrivate } from '../hooks/useAxiosPrivate';
 
 import {
   Button,
@@ -24,7 +24,10 @@ import {
   ModalHeader,
   TextInput,
   Select,
-  SelectItem
+  SelectItem,
+  InlineNotification,
+  Theme,
+  Checkbox
 } from '@carbon/react'
 
 import {
@@ -48,9 +51,18 @@ const Zones = function Zones() {
 
   const [newZoneName, setNewZoneName] = useState("")
   const [newZoneLoading, setNewZoneLoading] = useState(false)
+  const [newZoneError, setNewZoneError] = useState("");
+  const [deleteConfirmed, setDeleteConfirmed] = useState(false);
 
   if (error) return error.Message
   if (loading & !data) return <InlineLoading />
+
+  const handleCloseModal = (setOpen) => {
+    setNewZoneName("")
+    setNewZoneLoading(false)
+    setNewZoneError("")
+    setOpen(false)
+  }
 
   const modelAddSubmit = async (setOpen) => {
     setNewZoneLoading(true)
@@ -64,12 +76,14 @@ const Zones = function Zones() {
             headers: { 'Content-Type': 'application/json' }
           },
         );
-      } catch(error) {
-        console.error(error)
-      } 
+        setNewZoneError("");
+        handleCloseModal(setOpen);
+      } catch (error) {
+        console.error(error);
+        setNewZoneError(error.response.data.error);
+      }
     }
     setNewZoneLoading(false)
-    setOpen(false)
     refresh()
   }
 
@@ -89,40 +103,48 @@ const Zones = function Zones() {
               {loading && <InlineLoading />}
               <TableToolbarContent>
                 <Button renderIcon={Renew} hasIconOnly kind='secondary' iconDescription='Refresh' onClick={refresh}>Refresh Zones</Button>
-                {/* TODO ADD CREATE BUTTON */}
                 <StateManager
-                renderLauncher={({ setOpen }) => (
-                <Button renderIcon={Add} kind="primary" iconDescription="New Zone" onClick={() => setOpen(true)}>New Zone</Button> )}>
-                {({ open, setOpen }) => (
-                  <Modal
-                    modalLabel="DNS/Zones"
-                    modalHeading="New Zone"
-                    primaryButtonText={newZoneLoading ? "Creating" : "Create"}
-                    secondaryButtonText="Cancel"
-                    open={open}
-                    onRequestSubmit={() => modelAddSubmit(setOpen)}
-                    onRequestClose={() => setOpen(false)}>
-                    <p style={{ marginBottom: '1rem' }}>
-                      Create a new Zone to be managed by Connect
-                    </p>
-                    <TextInput
-                      data-modal-primary-focus
-                      id="zoneName"
-                      labelText="Zone name"
-                      placeholder="zone.vx0"
-                      style={{ marginBottom: '1rem' }}
-                      value={newZoneName}
-                      onChange={(event) => setNewZoneName(event.target.value)}
-                    />
-                    {/* error */}
-                    
-                    {/* TODO: IMPLEMENT ALLOCATION TO USER */}
-                    {/* <Select id="select-1" labelText="User">
+                  renderLauncher={({ setOpen }) => (
+                    <Button renderIcon={Add} kind="primary" iconDescription="New Zone" onClick={() => setOpen(true)}>New Zone</Button>)}>
+                  {({ open, setOpen }) => (
+                    <Modal
+                      modalLabel="DNS/Zones"
+                      modalHeading="New Zone"
+                      primaryButtonText={newZoneLoading ? "Creating" : "Create"}
+                      secondaryButtonText="Cancel"
+                      open={open}
+                      shouldSubmitOnEnter
+                      onRequestSubmit={() => modelAddSubmit(setOpen)}
+                      onRequestClose={() => handleCloseModal(setOpen)}>
+                      <p style={{ marginBottom: '1rem' }}>
+                        Create a new Zone to be managed by Connect
+                      </p>
+                      <TextInput
+                        data-modal-primary-focus
+                        id="zoneName"
+                        labelText="Zone name"
+                        placeholder="zone.vx0"
+                        style={{ marginBottom: '1rem' }}
+                        value={newZoneName}
+                        onChange={(event) => setNewZoneName(event.target.value)}
+                      />
+                      {/* TODO: IMPLEMENT ALLOCATION TO USER */}
+                      {/* <Select id="select-1" labelText="User">
                       <SelectItem value="all" text="All" /> 
                     </Select> */}
-                  </Modal>
-                )}
-              </StateManager>
+                      {newZoneError && (
+                        <InlineNotification
+                          title={newZoneError}
+                          // lowcontrast
+                          kind="error"
+                          lowContrast
+                        />
+                      )}
+                      <br />
+
+                    </Modal>
+                  )}
+                </StateManager>
               </TableToolbarContent>
             </TableToolbar>
             <Table size="lg" useZebraStyles={false}>
@@ -141,36 +163,53 @@ const Zones = function Zones() {
                     <TableCell>{zone.name}</TableCell>
                     <TableCell>{zone.created_at}</TableCell>
                     <TableCell>
-                    <StateManager
-                      renderLauncher={({ setOpen }) => (
-                        <Button
-                          renderIcon={TrashCan}
-                          iconDescription="Delete"
-                          hasIconOnly
-                          kind='ghost'
-                          onClick={() => setOpen(true)}
-                        />
-                      )}>
-                      {({ open, setOpen }) => (
-                         <Modal
-                          open={open}
-                          onRequestClose={() => setOpen(false)}
-                          onRequestSubmit={() => {
-                            modalDeleteSubmit(zone.id)
-                            setTimeout(() => {
-                              refresh()
-                              setOpen(false)
-                            }, 500)
-                          }}
-                          danger
-                          modalHeading={`Are you sure you want to delete zone ${zone.name}?`}
-                          modalLabel="DNS/Zones"
-                          primaryButtonText="Delete"
-                          secondaryButtonText="Cancel"
-                        />
-                      )}
-                    </StateManager>
-                    <Button
+                      <StateManager
+                        renderLauncher={({ setOpen }) => (
+                          <Button
+                            renderIcon={TrashCan}
+                            iconDescription="Delete"
+                            hasIconOnly
+                            kind='ghost'
+                            onClick={() => setOpen(true)}
+                          />
+                        )}>
+                        {({ open, setOpen }) => (
+                          <Modal
+                            open={open}
+                            onRequestClose={() => {
+                              setOpen(false);
+                              setDeleteConfirmed(false)
+                            }}
+                            onRequestSubmit={() => {
+                              if (deleteConfirmed) {
+                                modalDeleteSubmit(zone.id);
+                                setTimeout(() => {
+                                  refresh();
+                                  setOpen(false);
+                                  setDeleteConfirmed(false);
+                                }, 500);
+                              }
+                            }}
+                            danger
+                            modalHeading={`Are you sure you want to delete zone ${zone.name}?`}
+                            modalLabel="DNS/Zones"
+                            primaryButtonText="Delete"
+                            secondaryButtonText="Cancel"
+                            primaryButtonDisabled={!deleteConfirmed}
+                          >
+                            <Checkbox
+                              labelText="I confirm that I want to delete this zone and understand it cannot be recreated."
+                              id="deleteConfirmCheckbox"
+                              checked={deleteConfirmed}
+                              onChange={(event) =>
+                                setDeleteConfirmed(event.target.checked)
+                              }
+                            />
+                          </Modal>
+
+                        )}
+                      </StateManager>
+                      <Button
                         renderIcon={QueryQueue}
                         iconDescription="Records"
                         hasIconOnly
